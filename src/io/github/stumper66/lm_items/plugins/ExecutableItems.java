3b46f0fb1cb473bf3e0e05686b9be566f1a5e46c
Penal Buffalo <buffalo246@outlook.com>
2022-07-21 21:23:07 -0500

    v1.2.2
    * fixed crash if ExecutableItems wasn't installed

--- src/io/github/stumper66/lm_items/plugins/ExecutableItems.java
+++ src/io/github/stumper66/lm_items/plugins/ExecutableItems.java
@@ -4,17 +4,15 @@ import io.github.stumper66.lm_items.ExternalItemRequest;
 import io.github.stumper66.lm_items.GetItemResult;
 import io.github.stumper66.lm_items.ItemsAPI;
 import io.github.stumper66.lm_items.SupportsExtras;
-import io.github.stumper66.lm_items.Utils;
+import io.github.stumper66.lm_items.plugins.helpers.ExecutableItemsHelper;
 import org.bukkit.Bukkit;
 
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @SuppressWarnings("unused")
@@ -53,7 +51,7 @@ public class ExecutableItems implements ItemsAPI, SupportsExtras {
                         .getLoadedObjectWithID(itemRequest.itemId);
 
         oOpt.ifPresent(executableItem -> {
-            setExtras(executableItem, itemRequest.extras);
+            ExecutableItemsHelper.setExtras(executableItem, itemRequest.extras);
             final Optional<Integer> usageOpt = Optional.of(-147);
             final Optional<Player> playerOpt = Optional.empty();
             double useAmount = itemRequest.amount != null ? itemRequest.amount : 1.0;
@@ -61,53 +59,6 @@ public class ExecutableItems implements ItemsAPI, SupportsExtras {
         });
 
         return result;
-    }
-
-    private void setExtras(final @NotNull com.ssomar.testRecode.executableItems.NewExecutableItem ei, final @Nullable Map<String, Object> extras){
-        if (extras == null) return;
-
-        com.ssomar.scoretestrecode.features.custom.hiders.Hiders hiders = null;
-
-        final int usage = Utils.getIntValue(extras, "Usage", -2);
-
-        if (usage > -2)
-            ei.getUsage().setValue(Optional.of(usage));
-
-        final int usageLimit = Utils.getIntValue(extras, "UsageLimit", -2);
-        if (usageLimit > -2)
-            ei.getUsageLimit().setValue(Optional.of(usageLimit));
-
-        final int usePerDay = Utils.getIntValue(extras, "UsePerDay", -2);
-        if (usageLimit > -2)
-            ei.getUsePerDay().getMaxUsePerDay().setValue(Optional.of(usageLimit));
-
-        final int durability = Utils.getIntValue(extras, "Durability", -1);
-        if (durability > -1)
-            ei.getDurability().setValue(Optional.of(durability));
-
-        if ("true".equalsIgnoreCase(Utils.getValueString(extras, "HideUsage"))) {
-            hiders = new com.ssomar.scoretestrecode.features.custom.hiders.Hiders(ei);
-            hiders.getHideUsage().setValue(true);
-        }
-        if ("true".equalsIgnoreCase(Utils.getValueString(extras, "HideEnchantments"))) {
-            if (hiders == null) hiders = new com.ssomar.scoretestrecode.features.custom.hiders.Hiders(ei);
-            hiders.getHideEnchantments().setValue(true);
-        }
-        if ("true".equalsIgnoreCase(Utils.getValueString(extras, "HideAttributes"))) {
-            if (hiders == null) hiders = new com.ssomar.scoretestrecode.features.custom.hiders.Hiders(ei);
-            hiders.getHideAttributes().setValue(true);
-        }
-        if ("true".equalsIgnoreCase(Utils.getValueString(extras, "HideUnbreakable"))) {
-            if (hiders == null) hiders = new com.ssomar.scoretestrecode.features.custom.hiders.Hiders(ei);
-            hiders.getHideUnbreakable().setValue(true);
-        }
-        if ("true".equalsIgnoreCase(Utils.getValueString(extras, "HidePotionEffects"))) {
-            if (hiders == null) hiders = new com.ssomar.scoretestrecode.features.custom.hiders.Hiders(ei);
-            hiders.getHidePotionEffects().setValue(true);
-        }
-
-        if (hiders != null)
-            ei.setHiders(hiders);
     }
 
     public @NotNull Collection<String> getItemTypes() {
---
+++ src/io/github/stumper66/lm_items/plugins/helpers/ExecutableItemsHelper.java
@@ -0,0 +1,57 @@
+package io.github.stumper66.lm_items.plugins.helpers;
+
+import io.github.stumper66.lm_items.Utils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class ExecutableItemsHelper {
+    public static void setExtras(final @NotNull com.ssomar.testRecode.executableItems.NewExecutableItem ei, final @Nullable Map<String, Object> extras){
+        if (extras == null) return;
+
+        com.ssomar.scoretestrecode.features.custom.hiders.Hiders hiders = null;
+
+        final int usage = Utils.getIntValue(extras, "Usage", -2);
+
+        if (usage > -2)
+            ei.getUsage().setValue(Optional.of(usage));
+
+        final int usageLimit = Utils.getIntValue(extras, "UsageLimit", -2);
+        if (usageLimit > -2)
+            ei.getUsageLimit().setValue(Optional.of(usageLimit));
+
+        final int usePerDay = Utils.getIntValue(extras, "UsePerDay", -2);
+        if (usageLimit > -2)
+            ei.getUsePerDay().getMaxUsePerDay().setValue(Optional.of(usageLimit));
+
+        final int durability = Utils.getIntValue(extras, "Durability", -1);
+        if (durability > -1)
+            ei.getDurability().setValue(Optional.of(durability));
+
+        if ("true".equalsIgnoreCase(Utils.getValueString(extras, "HideUsage"))) {
+            hiders = new com.ssomar.scoretestrecode.features.custom.hiders.Hiders(ei);
+            hiders.getHideUsage().setValue(true);
+        }
+        if ("true".equalsIgnoreCase(Utils.getValueString(extras, "HideEnchantments"))) {
+            if (hiders == null) hiders = new com.ssomar.scoretestrecode.features.custom.hiders.Hiders(ei);
+            hiders.getHideEnchantments().setValue(true);
+        }
+        if ("true".equalsIgnoreCase(Utils.getValueString(extras, "HideAttributes"))) {
+            if (hiders == null) hiders = new com.ssomar.scoretestrecode.features.custom.hiders.Hiders(ei);
+            hiders.getHideAttributes().setValue(true);
+        }
+        if ("true".equalsIgnoreCase(Utils.getValueString(extras, "HideUnbreakable"))) {
+            if (hiders == null) hiders = new com.ssomar.scoretestrecode.features.custom.hiders.Hiders(ei);
+            hiders.getHideUnbreakable().setValue(true);
+        }
+        if ("true".equalsIgnoreCase(Utils.getValueString(extras, "HidePotionEffects"))) {
+            if (hiders == null) hiders = new com.ssomar.scoretestrecode.features.custom.hiders.Hiders(ei);
+            hiders.getHidePotionEffects().setValue(true);
+        }
+
+        if (hiders != null)
+            ei.setHiders(hiders);
+    }
+}
